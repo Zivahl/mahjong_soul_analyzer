9e4945fafa72bb4a8023c164648f5f605d93ed7e
Zivahl <gegayo.hofupi.03@gmail.com>
2026-07-26 00:12:46 +0900

    Fix use currentTsumo in DiscardModal

--- frontend/src/components/modal/ModalManager/ModalManager.tsx
+++ frontend/src/components/modal/ModalManager/ModalManager.tsx
@@ -273,7 +273,7 @@ export const ModalManager = () => {
                     }
         
                     tsumoTile={
-                        undefined
+                        state.currentTsumo
                     }
         
                     selectedTile={
